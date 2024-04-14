de3a8f1fe46773c5f2bad9456d9dfe96319ac4d6
Lasse Lautanala <lasselautanala@gmail.com>
2024-04-14 22:32:17 +0300

    Notes backend re-deploy

--- index.js
+++ index.js
@@ -1,118 +1,117 @@
 
 const express = require('express')
 const cors = require('cors')
-const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
-let persons = [
-  { 
-    name: "Arto Hellas", 
-    number: "040-123456",
-    id: 1
+let notes = [
+  {
+    id: 1,
+    content: "HTML is not easy",
+    important: true
   },
-  { 
-    name: "Ada Lovelace", 
-    number: "39-44-5323523",
-    id: 2
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
   },
-  { 
-    name: "Dan Abramov", 
-    number: "12-43-234345",
-    id: 3
-  },
-  { 
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122",
-    id: 4
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
   }
 ]
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
-
-  if (!body.name) {
-    return response.status(400).json({error: 'name missing'})
-  } else if (!body.number) {
-    return response.status(400).json({error: 'number missing'})
-  }
-  const oldPerson = persons.find(person => person.name === body.name)
-  if (oldPerson) {
-    return response.status(400).json({error: 'name must be unique'})
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
+  const note = {
+    content: body.content,
+    important: body.important || false,
     id: generateId(),
   }
-  persons = persons.concat(person)
-  response.json(person)
+  notes = notes.concat(note)
+  response.json(note)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
   } else {
     console.log('x')
     response.status(404).end()
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
 
-app.put('/api/persons/:id', (request, response) => {
+
+app.put('/api/notes/:id', (request, response) => {
+  console.log("PUT")
   const id = Number(request.params.id)
   const body = request.body
-  if (!body.name || !body.number) {
-    return response.status(400).json({error: 'name or number missing'})
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
   }
-  const changedPerson = persons.find(p => p.id === id)
-  if (changedPerson) {
-    changedPerson.name = body.name
-    changedPerson.number = body.number
-    response.json(changedPerson)
+  const changedNote = notes.find(n => n.id === id)
+  if (changedNote) {
+    changedNote.content = body.content
+    changedNote.important = body.important
+//    changedNote = { ...changedNote, important: !changedNote.important }
+//    notes = notes.map(note => note.id !== id ? note : changedNote)
+    response.json(changedNote)
   } else {
       console.log('x')
       response.status(404).end()
   }
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+//app.put('/api/notes/:id'', (request, response) => {
+//  const id = Number(request.params.id)
+//  const body = request.body
+//  if (!body.content) {
+//    return response.status(400).json({error: 'content missing'})
+//  }
+//
+//  const note = {
+//    content: body.content,
+//    important: body.important || false,
+//    id: generateId(),
+//  }
+//  notes = notes.concat(note)
+//  response.json(note)
+//})
+
+
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
