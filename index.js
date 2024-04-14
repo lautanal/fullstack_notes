a967008eff8926d5c4d4725507fff8e36aa94b53
Lasse Lautanala <lasselautanala@gmail.com>
2024-04-14 22:40:43 +0300

    Notes backend re-deploy

--- index.js
+++ index.js
@@ -94,24 +94,7 @@ app.put('/api/notes/:id', (request, response) => {
 })
 
 
-//app.put('/api/notes/:id'', (request, response) => {
-//  const id = Number(request.params.id)
-//  const body = request.body
-//  if (!body.content) {
-//    return response.status(400).json({error: 'content missing'})
-//  }
-//
-//  const note = {
-//    content: body.content,
-//    important: body.important || false,
-//    id: generateId(),
-//  }
-//  notes = notes.concat(note)
-//  response.json(note)
-//})
-
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
